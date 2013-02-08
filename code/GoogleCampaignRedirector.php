1ae8af1b390d7b271f0d6077ebf34f833ab933f9
Duttski <duttons.am@gmail.com>
2013-02-08 09:56:59 +0000

    Added commentary to GoogleCampaignRedirector

--- code/GoogleCampaignRedirector.php
+++ code/GoogleCampaignRedirector.php
@@ -1,6 +1,10 @@
 <?php
 
 class GoogleCampaignRedirector extends RedirectorPage {
+	
+	/*
+	 * The standard campaign tracking codes available
+	 */
 	public static $db = array(
 		"CampaignSource" => "Text",
 		"CampaignMedium" => "Text",
@@ -9,6 +13,9 @@ class GoogleCampaignRedirector extends RedirectorPage {
 		"CampaignName" => "Text"
 	);
 	
+	/*
+	 * Typically, these won't form part of a menu.
+	 */
 	public static $defaults = array(
 		"ShowInMenus" => 0,
 		"ShowInSearch" => 0
@@ -17,20 +24,26 @@ class GoogleCampaignRedirector extends RedirectorPage {
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
 		$fields->addFieldToTab('Root.Main', new LiteralField("ga_heading", "<hr /><h2>Google Analytics Campaign Settings</h2>"));
+		//TODO: Add a description of the /meaning/ of these to help users (or link through to Google's docs?)
 		$fields->addFieldToTab('Root.Main', new TextField('CampaignSource'));
 		$fields->addFieldToTab('Root.Main', new TextField('CampaignMedium'));
 		$fields->addFieldToTab('Root.Main', new TextField('CampaignTerm'));
 		$fields->addFieldToTab('Root.Main', new TextField('CampaignContent'));
 		$fields->addFieldToTab('Root.Main', new TextField('CampaignName'));
-		
 		return $fields;
 	}
 	
+	/**
+	 * For a given request, get the GA-ready query string that can be appended
+	 */
 	public function getQueryString() {
+		//These three are required no matter what
 		$cs = urlencode($this->CampaignSource);
 		$cm = urlencode($this->CampaignMedium);
 		$cn = urlencode($this->CampaignName);
 		$ret = "utm_source=$cs&utm_medium=$cm&utm_campaign=$cn";
+		
+		//The next two are optional so let's only set them if there's a value there.
 		if($this->CampaignTerm){
 			$ct = urlencode($this->CampaignTerm);
 			$ret .= "&utm_term=$ct";
@@ -46,12 +59,17 @@ class GoogleCampaignRedirector extends RedirectorPage {
 class GoogleCampaignRedirector_Controller extends RedirectorPage_Controller {
 	
 	public function init() {
-		if($link = self::addQueryString($this->redirectionLink(),$this->getQueryString())) {
-			$this->redirect($link, 301);
+		//Override the redirector behaviour to point to the GA-enabled link
+		if($link = $this->redirectionLink()) {
+			$this->redirect(self::addQueryString($link,$this->getQueryString()), 301);
 			return;
 		}
 	}
 	
+	/**
+	 * Helper function to add a query string to a URL in different ways depending on
+	 * the original URL. I.e. avoid example.com?a=b&c=d?e=f&g=h
+	 */
 	public static function addQueryString($link, $query){
 		if(strpos("?", $link) === FALSE){
 			return "$link?$query";
